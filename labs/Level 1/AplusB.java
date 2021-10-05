e481465929b2f85f9aa837e71b1d05b56b80554e
96hckim <yp050431@gmail.com>
2021-10-05 10:55:14 +0900

    A + B

---
+++ labs/Level 1/AplusB.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * AplusB
+ */
+public class AplusB {
+
+  public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+    int a = Integer.parseInt(scan.next());
+    int b = Integer.parseInt(scan.next());
+
+    System.out.print(a+b);
+
+  }
+  
+}
