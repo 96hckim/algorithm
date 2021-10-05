2986526d5120b69a904c8d49e559918cb14a7b38
96hckim <yp050431@gmail.com>
2021-10-05 13:11:48 +0900

    소수 판별

---
+++ labs/Level 1/PrimeNumber.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * PrimeNumber
+ */
+public class PrimeNumber {
+
+  public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+    int n = scan.nextInt();
+    int count = 0;
+    String result;
+
+    for (int i = 1; i <= n; i++) {
+      if (n % 1 == 0) {
+        count++;
+      }
+    }
+
+    if (count == 2) {
+      result = "YES";
+    } else {
+      result = "NO";
+    }
+
+    System.out.println(result);
+    scan.close();
+
+  }
+  
+}
