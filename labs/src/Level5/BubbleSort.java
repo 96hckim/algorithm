385548d0d83b7342ceb6ab6b0d6041b85a651170
96hckim <yp050431@gmail.com>
2021-10-08 17:28:41 +0900

    버블정렬

---
+++ labs/src/Level5/BubbleSort.java
@@ -0,0 +1,34 @@
+package Level5;
+
+import java.io.*;
+
+public class BubbleSort {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = {1, 4, 2, 5};
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int num : arr) {
+            bw.write(num + " ");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+
+    }
+
+}
