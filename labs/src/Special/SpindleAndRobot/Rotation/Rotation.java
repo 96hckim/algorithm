232c46ddbcefec9b42997f7b45a63cda5fe0d2fa
96hckim <yp050431@gmail.com>
2021-10-08 13:32:51 +0900

    상하좌우 SHIFT 알고리즘

--- labs/src/Special/SpindleAndRobot/Rotation/Rotation.java
+++ labs/src/Special/SpindleAndRobot/Rotation/Rotation.java
@@ -10,7 +10,6 @@ public class Rotation {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int q = Integer.parseInt(st.nextToken());
---
+++ labs/src/Special/SpindleAndRobot/move/DirectionShift.java
@@ -0,0 +1,47 @@
+package Special.SpindleAndRobot.move;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class DirectionShift {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        String[][] arr = new String[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = st.nextToken();
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int y = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+
+        for (int i = 0; i < r; i++) {
+            x += dx[d];
+            y += dy[d];
+        }
+
+        bw.write(arr[y][x]);
+
+        br.close();
+        bw.flush();
+        bw.close();
+
+    }
+
+}
