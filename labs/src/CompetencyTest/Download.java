fc55de12a9000e90f3876ee3be1054b15b8c6610
96hckim <yp050431@gmail.com>
2021-11-22 11:39:40 +0900

    다운로드

--- labs/src/CompetencyTest/Download.java
+++ labs/src/CompetencyTest/Download.java
@@ -1,6 +1,8 @@
 package CompetencyTest;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Download {
@@ -8,7 +10,6 @@ public class Download {
     private static class Person {
 
         int y, x, d;
-        AP used = new AP();
 
         public Person(int y, int x, int d) {
             this.y = y;
@@ -30,10 +31,7 @@ public class Download {
 
     private static class AP {
 
-        int y, x, r, d = 0;
-
-        public AP() {
-        }
+        int y, x, r, d;
 
         public AP(int y, int x, int r, int d) {
             this.y = y;
@@ -42,20 +40,6 @@ public class Download {
             this.d = d;
         }
 
-        @Override
-        public boolean equals(Object obj) {
-            AP ap = (AP) obj;
-            return this.y == ap.y && this.x == ap.x && this.r == ap.r;
-        }
-
-        @Override
-        public String toString() {
-            return "{" + y +
-                    ", " + x +
-                    ", " + r +
-                    ", " + d +
-                    '}';
-        }
     }
 
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 읽기
@@ -65,6 +49,7 @@ public class Download {
     private static int M; // 지도의 가로 크기
     private static int K; // 공유기의 개수
     private static AP[][][] map; // 지도
+    private static int[][] size; // 좌표의 AP 갯수
     private static int S; // A와 B의 이동 길이
     private static int[] moveA; // A의 이동 경로
     private static int[] moveB; // B의 이동 경로
@@ -79,7 +64,8 @@ public class Download {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        map = new AP[N + 1][M + 1][5];
+        map = new AP[N + 1][M + 1][K];
+        size = new int[N + 1][M + 1];
 
         // 지도 초기화
         for (int i = 0; i < K; i++) {
@@ -91,21 +77,16 @@ public class Download {
             setSpeed(new AP(y, x, r, d));
         }
 
-
         // A, B 이동 거리 초기화
         S = Integer.parseInt(br.readLine());
 
         moveA = new int[S + 1];
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= S; i++) {
-            moveA[i] = Integer.parseInt(st.nextToken());
-        }
+        for (int i = 1; i <= S; i++) moveA[i] = Integer.parseInt(st.nextToken());
 
         moveB = new int[S + 1];
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= S; i++) {
-            moveB[i] = Integer.parseInt(st.nextToken());
-        }
+        for (int i = 1; i <= S; i++) moveB[i] = Integer.parseInt(st.nextToken());
 
         // A, B 초기화
         st = new StringTokenizer(br.readLine());
@@ -131,16 +112,10 @@ public class Download {
                 if (j < 1) continue;
                 else if (j > M) break;
 
-                if (Math.abs(ap.y - i) + Math.abs(ap.x - j) <= ap.r) {
-                    for (int k = 0; k < 5; k++) {
-                        if (map[i][j][k] == null) {
-                            map[i][j][k] = ap;
-                            break;
-                        }
-                    }
-                }
+                if (Math.abs(ap.y - i) + Math.abs(ap.x - j) <= ap.r) map[i][j][size[i][j]++] = ap;
             }
         }
+
     }
 
     // 출력
@@ -166,52 +141,71 @@ public class Download {
 
     }
 
+    // A, B 이동
     private static void moving() {
         for (int s = 0; s <= S; s++) {
             A.move(moveA[s]);
             B.move(moveB[s]);
-
-            AP max = new AP();
-
-            for (AP ap : map[A.y][A.x]) {
-                if (ap == null) break;
-                if (B.used.equals(ap)) {
-                    if (max.d < (ap.d / 2)) {
-                        max = new AP(ap.y, ap.x, ap.r, ap.d);
-                    }
-                } else {
-                    if (max.d < ap.d) {
-                        max = new AP(ap.y, ap.x, ap.r, ap.d);
-                    }
-                }
-            }
-
-            A.used = max;
-
-            max = new AP();
-            for (AP ap : map[B.y][B.x]) {
-                if (ap == null) break;
-                if (A.used.equals(ap)) {
-                    if (max.d < (ap.d / 2)) {
-                        max = new AP(ap.y, ap.x, ap.r, ap.d);
-                    }
-                } else {
-                    if (max.d < ap.d) {
-                        max = new AP(ap.y, ap.x, ap.r, ap.d);
-                    }
-                }
-            }
-
-            B.used = max;
-
-            downloading(A, B);
-            downloading(B, A);
+            selectAP();
         }
     }
 
-    private static void downloading(Person person, Person other) {
-        if (person.used.equals(other.used)) person.download(person.used.d / 2);
-        else person.download(person.used.d);
+    // 공유기 선택
+    private static void selectAP() {
+        ArrayList<AP> apA = new ArrayList<>(size[A.y][A.x]); // A 좌표 공유기 리스트
+        ArrayList<AP> apB = new ArrayList<>(size[B.y][B.x]); // B 좌표 공유기 리스트
+
+        apA.addAll(Arrays.asList(map[A.y][A.x]).subList(0, size[A.y][A.x]));
+        apB.addAll(Arrays.asList(map[B.y][B.x]).subList(0, size[B.y][B.x]));
+
+        int max = Integer.MIN_VALUE;
+        int downloadA = 0; // A 가 다운로드 받을 용량
+        int downloadB = 0; // B 가 다운로드 받을 용량
+
+        if (apA.size() > 0 && apB.size() > 0) { // A 좌표와 B 좌표 둘다 공유기가 1개 이상 있을때
+            for (AP a : apA) {
+                for (AP b : apB) {
+                    int ad = a.d;
+                    int bd = b.d;
+                    int sum = ad + bd;
+
+                    if (a.equals(b)) { // 같은 공유기라면 속도 절반
+                        ad /= 2;
+                        bd /= 2;
+                        sum /= 2;
+                    }
+
+                    if (max < sum) { // 가장 많은 용량 다운로드
+                        max = sum;
+                        downloadA = ad;
+                        downloadB = bd;
+                    } else if (max == sum) { // 용량이 같다면 다운로드 받을 용량 많이 남은 쪽이 큰 경우
+                        if (A.d >= B.d) { // A 의 다운로드 받을 용량이 크거나 같으면 A 가 큰 용량 선택
+                            if (ad > downloadA) {
+                                downloadA = ad;
+                                downloadB = bd;
+                            }
+                        } else {
+                            if (bd > downloadB) {
+                                downloadA = ad;
+                                downloadB = bd;
+                            }
+                        }
+                    }
+                }
+            }
+        } else { // 둘다 좌표에 공유기가 없거나 한 쪽이 없는 경우
+            for (AP a : apA) if (downloadA < a.d) downloadA = a.d;
+            for (AP b : apB) if (downloadB < b.d) downloadB = b.d;
+        }
+
+        downloading(downloadA, downloadB);
+    }
+
+    // 지정된 용량 다운로드
+    private static void downloading(int downloadA, int downloadB) {
+        A.download(downloadA);
+        B.download(downloadB);
     }
 
 }
