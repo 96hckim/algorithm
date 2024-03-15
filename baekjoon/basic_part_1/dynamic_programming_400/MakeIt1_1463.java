ef889c9b0c5effe37b448f709c9103f07739365b
96hckim <yp050431@gmail.com>
2024-03-16 00:36:40 +0900

    1463 - 1로 만들기

---
+++ baekjoon/basic_part_1/dynamic_programming_400/MakeIt1_1463.java
@@ -0,0 +1,28 @@
+package basic_part_1.dynamic_programming_400;
+
+import java.io.*;
+
+public class MakeIt1_1463 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1];
+
+        dp[0] = dp[1] = 0;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        }
+
+        bw.write(dp[N] + "");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
